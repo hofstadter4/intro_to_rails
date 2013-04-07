0288f0408b2f4f788520e6c0147b15b14687c49b
JP Allen <jpallen@JP-Allens-MacBook-Air.local>
2013-04-07 16:26:22 -0400

    fifth commit, plus tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 IntroToRails::Application.routes.draw do
+  resources :tweets
+
+
+  get "welcome/index"
+
   get "demo/hello"
   get "demo/hello/:name" => "demo#hello"
 
@@ -51,7 +56,7 @@ IntroToRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
